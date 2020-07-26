4f1084551a919750cc6da63dda1f197da5a02fac
Trong Tran <trong.tn178@gmail.com>
2020-07-26 10:34:13 +0700

    Commit changes

--- Assets/Scripts/MainMenuButtonScript/QuitGame.cs
+++ Assets/Scripts/MainMenuButtonScript/QuitGame.cs
@@ -1,8 +1,23 @@
-﻿using System;
+﻿using System.IO;
+using Assets.Scripts.SaveSystem;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 namespace Assets.Scripts.MainMenuButtonScript
 {
-    public class QuitGame
+    public class QuitGame : MonoBehaviour
     {
+        public Button yourButton;
+        void Start()
+        {
+            Button btn = yourButton.GetComponent<Button>();
+            btn.onClick.AddListener(QuitGameScript);
+        }
 
+        void QuitGameScript()
+        {
+            Debug.Log("Quit");
+            Application.Quit();
+        }
     }
 }
